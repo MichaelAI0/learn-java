214b5484c4f3c2a0271f356e8e27c1aed5ee2284
MichaelAI0 <99698667+MichaelAI0@users.noreply.github.com>
2023-07-01 00:03:11 -0500

    Java Print Variables

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("I am learning Java.");
+    System.out.println("It is awesome! ");
+    // words are added to the same line using print
+    System.out.print("Hello World! ");
+    System.out.println("I will print on the same line.");
+    // this is a single line comment
+    // You can do math operations in java
+    System.out.println(3 + 3);
+    /*
+     * this is a
+     * multi-line comment
+     */
+    // you can reassign variables in java
+    // you can add a variable to a string value in println
+    String name = "John";
+    System.out.println("Hello " + name);
+
+  }
+}
